8bd8d146bc6a6f04a965d77a7ee32652fe1df2b3
Pranjal Prasun <pranjalprasun@Pranjals-MacBook-Air.local>
2022-02-13 12:43:57 +0530

    v0.0.4

--- src/app/Models/IQuestionBank.ts
+++ src/app/Models/IQuestionBank.ts
@@ -1,18 +1,14 @@
 export interface IQuestionBank {
-  questionBankID: string;
-  testItemNo: string;
-  nosCode: string;
-  nosName: string;
-  pcElement: string;
-  testItemSteam: string;
-  testOption1: string;
-  testOption2: string;
-  testOption3: string;
-  testOption4: string;
-  testOption5: string;
-  correctTestOptionChar: string;
-  correctTestOptionNum: string;
-  testOptionTypr: string;
-  noOfOption: string;
-  createdOn: Date;
+  questionBankID: string
+  testItemNo: string
+  nosCode: string
+  nosName: string
+  pcElement: string
+  testItemSteam: string
+  noOfOptions: string
+  testOptions: string[]
+  correctTestOption: string
+  testOptionType: string
+  noOfOption: string
+  createdOn: Date
 }
