30dc95c995710b7107e4050ed3d0033e852f2c09
sanni@jobulary.io <sanni@leatherssc.org>
2022-02-13 10:37:53 +0530

    0.0.3

---
+++ src/app/Models/IQuestionBank.ts
@@ -0,0 +1,18 @@
+export interface IQuestionBank {
+  questionBankID: string;
+  testItemNo: string;
+  nosCode: string;
+  nosName: string;
+  pcElement: string;
+  testItemSteam: string;
+  testOption1: string;
+  testOption2: string;
+  testOption3: string;
+  testOption4: string;
+  testOption5: string;
+  correctTestOptionChar: string;
+  correctTestOptionNum: string;
+  testOptionTypr: string;
+  noOfOption: string;
+  createdOn: Date;
+}
