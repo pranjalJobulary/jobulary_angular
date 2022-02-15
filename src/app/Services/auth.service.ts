271400f5178e62fe46e2f097d372246db5209092
Pranjal Prasun <pranjalprasun@Pranjals-MacBook-Air.local>
2022-02-15 17:38:30 +0530

    v0.1.10

--- src/app/Services/auth.service.ts
+++ src/app/Services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
 
   userId!: string
 
-  constructor(private authAngular: AngularFireAuth, private userDetails: TimelinePostServiceService, private route: Router) { }
+  constructor(private authAngular: AngularFireAuth, private route: Router) { }
 
   signIn(email: string, password: string){
     this.authAngular.signInWithEmailAndPassword(email, password)
@@ -21,7 +21,6 @@ export class AuthService {
       console.log('Sucess', value.user?.uid),
       console.log(value.additionalUserInfo?.providerId);
       console.log(this.userId);
-      this.userDetails.id = this.userId;
       this.route.navigate(['located']);
       return "Success";
     }).catch(error => {
